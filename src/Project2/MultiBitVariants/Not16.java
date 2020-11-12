53b6fc70428df83391d951b967468b5585b13f86
appleby <alexappleby70@gmail.com>
2020-11-12 14:10:50 -0700

    did remaining 16b gates unsophisticatedly

--- src/Project2/MultiBitVariants/Not16.java
+++ src/Project2/MultiBitVariants/Not16.java
@@ -1,15 +1,22 @@
 package Project2.MultiBitVariants;
 
 
+import Project2.ElementaryLogicGates.Not;
+
 public class Not16 {
     public boolean[] out = new boolean[16];
 
+    public Not[] nots = new Not[16];
 
     public Not16() {
-
+        for (int i = 0; i < nots.length; i++) {
+            nots[i] = new Not();
+        }
     }
 
     public void compute(boolean[] in){
-
+        for (int i = 0; i < nots.length; i++) {
+            out[i] = nots[i].calc(in[i]);
+        }
     }
 }
